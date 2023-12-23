c4264910ed26e0d1227a0c6cd9e4e6e2fb8a4d3d
Hareesh P <46471092+hareeshp007@users.noreply.github.com>
2023-12-23 17:36:32 +0530

    Third person movement completed

--- UnityDeveloper_Test/Assets/Scripts/Player/PlayerView.cs
+++ UnityDeveloper_Test/Assets/Scripts/Player/PlayerView.cs
@@ -6,11 +6,9 @@ using UnityEngine;
 public class PlayerView : MonoBehaviour
 {
     public Animator Playeranimator;
-    public Transform Camera;
+    public Transform Camerapos;
     [SerializeField]
     private CharacterController playerController;
-    [SerializeField]
-    private Rigidbody playerrb;
     [SerializeField]
     private float horizontal;
     [SerializeField]
@@ -23,13 +21,18 @@ public class PlayerView : MonoBehaviour
     private float Gravity;
     [SerializeField]
     private Vector3 velocity;
+    [SerializeField]
     private float turnSmoothTime=0.1f;
+    [SerializeField]
+    private float targetAngle;
+    [SerializeField]
+    private float angle;
+
     float turnSmoothVelocity;
 
     private void Start()
     {
         playerController=GetComponent<CharacterController>();
-        playerrb = GetComponent<Rigidbody>();
         horizontal = 0;
         vertical = 0;
     }
@@ -42,7 +45,7 @@ public class PlayerView : MonoBehaviour
 
     private void manualPhysics()
     {
-        if (velocity.y < 50f)
+        if (velocity.y < 10f)
         {
             velocity.y += Gravity * Time.deltaTime;
             playerController.Move(velocity * Time.deltaTime);
@@ -67,16 +70,23 @@ public class PlayerView : MonoBehaviour
     private void movement()
     {
         Vector3 newpos = new Vector3(horizontal, 0f, vertical).normalized;
-
+        float offsetangle = Camerapos.eulerAngles.y;
+        if(Camerapos.eulerAngles.y <= 180)
+        {
+            offsetangle += 180;
+        }
+        else
+        {
+            offsetangle -= 180;
+        }
         if (newpos.magnitude >= 0.1f)
         {
-            Debug.Log(newpos.magnitude);
-            float targetAngle = MathF.Atan2(newpos.x, newpos.z) * Mathf.Rad2Deg + Camera.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.rotation.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            targetAngle = Mathf.Atan2(newpos.x, newpos.z) * Mathf.Rad2Deg + offsetangle;
+            angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            playerController.Move(moveDir.normalized * speed * Time.deltaTime);
+            playerController.Move(moveDir.normalized * -speed * Time.deltaTime);
             Playeranimator.SetFloat("speed", newpos.magnitude);
         }
         else
